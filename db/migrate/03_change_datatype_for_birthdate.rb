df11998d668c954345ff883dfbcb9a79942865bb
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-06 09:54:08 +0000

    Automatically backed up by Learn

---
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -0,0 +1,9 @@
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.string :birthdate
+      t.integer :grade
+    end
+  end
+end
